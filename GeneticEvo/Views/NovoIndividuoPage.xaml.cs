47c960e360481d8c17713a5c42f026c0d4a62763
Guilherme-Ventura <guilherme.almeida.ventura@gmail.com>
2023-04-19 13:40:08 -0300

    Criado formulario do NovoIndividuo e  feito validadacao de campos vazios

--- GeneticEvo/Views/NovoIndividuoPage.xaml.cs
+++ GeneticEvo/Views/NovoIndividuoPage.xaml.cs
@@ -17,5 +17,22 @@ public partial class NovoIndividuoPage : ContentPage
         NovoIndividuo = new Individuo();
     }
 
-
+    private void Button_Confirmar(object sender, EventArgs e)
+    {
+        if (string.IsNullOrWhiteSpace(inputNome.Text) ||
+        string.IsNullOrWhiteSpace(inputEspecie.Text) ||
+        string.IsNullOrWhiteSpace(inputFiliacao.Text) ||
+        string.IsNullOrWhiteSpace(inputVida.Text) ||
+        string.IsNullOrWhiteSpace(inputFome.Text) ||
+        string.IsNullOrWhiteSpace(inputEnergia.Text) ||
+        string.IsNullOrWhiteSpace(inputTempoVida.Text) ||
+        string.IsNullOrWhiteSpace(inputOrigem.Text))
+        {
+            DisplayAlert("Erro", "Por favor preencha todos os campos.", "OK");
+        }
+        else
+        {
+            DisplayAlert("Sucesso", "Seus dados foram enviados!", "ok");
+        }
+    }
 }
