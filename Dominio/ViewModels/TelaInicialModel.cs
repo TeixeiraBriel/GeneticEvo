04e405bda1ef28b57f1161ed26be40dbe90f3b7e
Guilherme-Ventura <guilherme.almeida.ventura@gmail.com>
2024-02-01 21:53:40 -0300

    Correcao do local model

--- Dominio/ViewModels/TelaInicialModel.cs
+++ Dominio/ViewModels/TelaInicialModel.cs
@@ -1,11 +1,4 @@
-﻿using GeneticEvo.Entidades.Caracteristicas;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace GeneticEvoBlazor.Model
+﻿namespace Dominio.ViewModels
 {
     public class TelaInicialModel
     {
