6778697b4b86bc6bc4398bbf521203e81a82f88c
Gabriel Teixeira <Gabriel Teixeira@GABRIELTPC>
2024-02-01 22:03:39 -0300

    Teste

--- Dominio/ViewModels/TelaInicialModel.cs
+++ Dominio/ViewModels/TelaInicialModel.cs
@@ -4,7 +4,7 @@
     {
         public TelaInicialModel()
         {
-            NomeJogador = "Takato";
+            NomeJogador = "Gabriel";
             Conquistas = new List<string>() { "100 ind", "100 geracoes" };
         }
 
